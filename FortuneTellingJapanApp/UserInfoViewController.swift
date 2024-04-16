bb35faedd74ddbb74d2fa85c69af64e50ff47ab2
Gomichan10 <cckg1220113@gn.iwasaki.ac.jp>
2024-04-16 13:51:57 +0900

    UIDatePickerから誕生日を受け取れる関数を作成した

---
+++ FortuneTellingJapanApp/UserInfoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UserInfoViewController.swift
+//  FortuneTellingJapanApp
+//
+//  Created by Gomi Kouki on 2024/04/16.
+//
+
+import UIKit
+
+class UserInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
