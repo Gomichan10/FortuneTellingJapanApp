03993c36ffe6edd5a12c4160e7a85612ed2ca7bd
Gomichan10 <cckg1220113@gn.iwasaki.ac.jp>
2024-04-16 14:03:52 +0900

    UIPickerViewで血液型を選択できるようにした

--- FortuneTellingJapanApp/UserInfoViewController.swift
+++ FortuneTellingJapanApp/UserInfoViewController.swift
@@ -8,22 +8,54 @@
 import UIKit
 
 class UserInfoViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var bloodPicker: UIPickerView!
+    
+    var year = 0
+    var month = 0
+    var day = 0
+    
+    let bloodType = ["a", "b", "ab", "o"]
+    var userBloodType = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        bloodPicker.dataSource = self
+        bloodPicker.delegate = self
+        
+        datePicker.addTarget(self, action: #selector(dateChanged(_:)), for: .valueChanged)
+    }
+    
+    @objc func dateChanged(_ sender: UIDatePicker) {
+        let calendar = Calendar.current
+        year = calendar.component(.year, from: sender.date)
+        month = calendar.component(.month, from: sender.date)
+        day = calendar.component(.day, from: sender.date)
+        
+        print(year,month,day)
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension UserInfoViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-    */
-
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return bloodType.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return bloodType[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        userBloodType = bloodType[row]
+        print(userBloodType)
+    }
 }
