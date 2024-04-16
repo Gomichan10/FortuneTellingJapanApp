fd47fb25071b258242caeed413868e0b20b1e9d3
Gomichan10 <cckg1220113@gn.iwasaki.ac.jp>
2024-04-16 13:30:01 +0900

    Initial Commit

---
+++ FortuneTellingJapanApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FortuneTellingJapanApp
+//
+//  Created by Gomi Kouki on 2024/04/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
